c44cd2e05be55ad85705b9441534c427afa96fdc
Yash Choudhary <112472346+Fibbzyash@users.noreply.github.com>
2024-05-02 19:49:54 +0530

    Update arrays1.cpp

--- arrays1.cpp
+++ arrays1.cpp
@@ -1,1 +1,5 @@
 //First day of learning Arrays
+//Largest Element in an Array
+//Second Largest Element in an Array without sorting
+//Check if the array is sorted
+//Remove duplicates from Sorted array
