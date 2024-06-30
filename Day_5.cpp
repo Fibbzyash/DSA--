0155f5b53b17ab9a20f9fdffbc55943748470a4c
Yash Choudhary <112472346+Fibbzyash@users.noreply.github.com>
2024-06-30 17:20:15 +0530

    Update Day_5.cpp

--- Day_5.cpp
+++ Day_5.cpp
@@ -141,6 +141,26 @@ int maxSubArraySum(int arr[], int n) {
             sum = 0;
         }
     }
-    return maxi;  // Return the maximum subarray sum
+    return maxi;  
 }
 
+//Best Time to Buy and Sell Stock | DP on Stocks 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0) return 0; 
+
+        int mini = prices[0];
+        int maxProfit = 0;
+        
+        for (int i = 1; i < n; ++i) {
+
+            maxProfit = max(maxProfit, prices[i] - mini);
+            
+            mini = min(mini, prices[i]);
+        }
+        
+        return maxProfit;
+    }
+};
