1e2d94b8067cfef7fa075939c237d11ece43bf89
Yash Choudhary <112472346+Fibbzyash@users.noreply.github.com>
2024-07-01 21:19:21 +0530

    Create Day_6.cpp

---
+++ Day_6.cpp
@@ -0,0 +1,26 @@
+//Rearrange Array Elements by Sign
+//TS =(0)n
+
+void rearrange(int arr[], int n) {
+    int pos[n / 2];
+    int neg[n / 2];
+    int posIndex = 0, negIndex = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 0) {
+            pos[posIndex++] = arr[i];
+        } else {
+            neg[negIndex++] = arr[i];
+        }
+    }
+
+    
+    int index = 0;
+    posIndex = 0;
+    negIndex = 0;
+    
+    while (posIndex < n / 2 && negIndex < n / 2) {
+        arr[index++] = pos[posIndex++];
+        arr[index++] = neg[negIndex++];
+    }
+}
